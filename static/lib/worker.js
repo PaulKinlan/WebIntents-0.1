7854dc78921e61881de767b38c98eb0beacba90c
Paul Kinlan <paulkinlan@paulkinlan-macbookpro.local>
2010-12-17 09:37:11 +0000

    Removing redundant files

--- static/lib/worker.js
+++
@@ -1,8 +0,0 @@
-onconnect = function(e) {
-  console.log("connected");
-  var port = e.ports[0];
-  console.log("ports: " + e.ports.length);
-  port.addEventListener("message", function(m) {
-    console.log(m)
-  });
-};
